3d45d932b7a7e34e1e65935b1d7090bafb6efec0
cherlasrisai-k <cherlasrisai.k@encora.com>
2025-10-10 15:52:53 +0530

    Inter service communication Synchronous (RestTemplate)

---
+++ 10th-Oct-2025/Emp-Order-Servicde/src/test/java/com/orderTest/Emp_Order_Servicde/EmpOrderServicdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderTest.Emp_Order_Servicde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpOrderServicdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
